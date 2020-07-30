0d8ed3410d0d8d7f108e8822573793a09c470b22
Mounika475 <67385748+Mounika475@users.noreply.github.com>
2020-07-30 15:48:40 +0530

    Update healthCheck.groovy

--- vars/healthCheck.groovy
+++ vars/healthCheck.groovy
@@ -1,1 +1,16 @@
-
+//def call(Map config) {
+    //node {
+   //   checkout([
+//$class: 'GitSCM',
+//branches: [[name: stageParams.branch ]],
+//userRemoteConfigs: [[ url: stageParams.url]]
+//sh 'mvn package'
+//])
+        //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
+ def build(){
+    stage("complilation"){
+        sh "mvn package" 
+        
+        //mail to: '...', subject: "${config.name} plugin build", body: '...'
+    }
+}
