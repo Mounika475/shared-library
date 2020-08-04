7c924b5d336b5cedcfee31f3e8b43383524756a4
Mounika475 <67385748+Mounika475@users.noreply.github.com>
2020-08-04 17:23:51 +0530

    Update Environment.groovy

--- vars/Environment.groovy
+++ vars/Environment.groovy
@@ -1,15 +1,31 @@
 def call(){
+    //4)
+    pipeline {
+    agent any
+    stages {
+        stage('clone') {
+            steps {
+                echo "Database engine is ${DB_ENGINE}"
+                echo "DISABLE_AUTH is ${DISABLE_AUTH}"
+               // sh 'printenv'
+            }
+        }
+    }
+}
+    
+    
+    
     //   1)echo ("FOO: ${FOO}")
    //      echo ("FOO:"+env.FOO)
     
     
-   //2) String myVar = evaluate("myVar")
+  
+    //2) String myVar = evaluate("myVar")
     
-    //3)
+   
     
-
-    println(this.env.FOO)
-    println(env.FOO)
+    //3) println(this.env.FOO)
+       //println(env.FOO)
 
 
 
