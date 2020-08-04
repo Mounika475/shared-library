26947e037f14f93cde822d8cd4a14a6c333b0ee0
Mounika475 <67385748+Mounika475@users.noreply.github.com>
2020-08-04 12:54:21 +0530

    Update Environment.groovy

--- vars/Environment.groovy
+++ vars/Environment.groovy
@@ -1,6 +1,16 @@
 def call(){
-    //echo ("FOO: ${FOO}")
-   // echo ("FOO:"+env.FOO)
-    String myVar = evaluate("myVar")
+    //   1)echo ("FOO: ${FOO}")
+   //      echo ("FOO:"+env.FOO)
+    
+    
+   //2) String myVar = evaluate("myVar")
+    
+    //3)
+    
+
+    println(this.env.FOO)
+    println(env.FOO)
+
+
 
 }
