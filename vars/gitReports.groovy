6ff08dd8610d8f6537adf768e4f0402b88d2a916
Mounika475 <67385748+Mounika475@users.noreply.github.com>
2020-08-05 14:47:41 +0530

    Update gitReports.groovy

--- vars/gitReports.groovy
+++ vars/gitReports.groovy
@@ -1,1 +1,11 @@
+def call(Map config){    
+    
+     stage ("Reports"){
+        jacoco exclusionPattern: '**/*Test*.class', inclusionPattern: '**/*.class', sourceExclusionPattern: '/**/*.java'
+         pmd canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/pmd.xml.', unHealthy: ''
+          
+     
+       
+    
+}
 
