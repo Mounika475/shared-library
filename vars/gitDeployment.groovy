7a914fe0a3f64ee0b728d1a7441297dfc3b91011
Mounika475 <67385748+Mounika475@users.noreply.github.com>
2020-08-10 16:48:42 +0530

    Update gitDeployment.groovy

--- vars/gitDeployment.groovy
+++ vars/gitDeployment.groovy
@@ -1,6 +1,6 @@
 def call(Map config){
      
-sh "curl -v -u tomcat:tomcat -T /root/.jenkins/workspace/shared1/target/spring3-mvc-maven-xml-hello-world-3.0-SNAPSHOT.war 'http://ec2-18-216-94-232.us-east-2.compute.amazonaws.com:8080//manager/text/deploy?path=/gradle1'"
+sh "curl -v -u tomcat:tomcat -T /root/.jenkins/workspace/shared1/target/spring3-mvc-maven-xml-hello-world-3.0-SNAPSHOT.war 'http://ec2-18-222-145-151.us-east-2.compute.amazonaws.com:8080//manager/text/deploy?path=/gradle1'"
    
     
 }
